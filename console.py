d745150ff4d7f79b9395cfb881cc24ec5c224b8a
Dobroslav Tokoš <dtokos@zoznam.sk>
2019-09-24 09:32:08 +0200

    Implemented dynamic IO

---
+++ console.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+def header(title):
+	print(u'----- {} -----'.format(title))
+
+def stat(message, current, max):
+	print('{} {}/{} ({:.2%})'.format(message, current, max, float(current) / float(max) if max else 0))
+
+def confirm(message, defaultYes = False):
+	acceptedAnswers = ['yes', 'ye', 'y']
+	answer = choice(message, acceptedAnswers + ['no', 'n'], 'yes' if defaultYes else 'no')
+	
+	return answer in acceptedAnswers
+
+def choice(message, options, default = None, invalidMessage = 'Value you entered is invalid'):
+	answer = prompt(message, default).lower()
+
+	while answer not in options:
+		print(invalidMessage)
+		answer = prompt(message, default).lower()
+
+	return answer
+
+def prompt(message, default = None):
+	if default:
+		message = '{} ({}): '.format(message, default)
+	else:
+		message = '{}: '.format(message)
+
+	return raw_input(message) or default
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
 import glob
+import console
 from PIL import Image
 from classifiers import AvgRGBClassifier
 from Builder import Builder
@@ -7,8 +9,12 @@ from Generator import Generator
 from calculators import AvgDeltaCalculator, WeightedRGBDeltaCalculator, WeightedHSVDeltaCalculator
 
 def main():
-	images = getTileImages()
-	source = Image.open('source.png')
+	imagesPath = console.prompt('Enter images path', './images/')
+	sourceName = console.prompt('Enter source', 'source.png')
+	outputName = console.prompt('Enter source', 'output.png')
+
+	images = getTileImages(imagesPath)
+	source = Image.open(sourceName)
 	classifier = AvgRGBClassifier()
 	deltaCalculator = (
 		AvgDeltaCalculator()
@@ -17,10 +23,10 @@ def main():
 	)
 	builder = Builder((56, 56))
 	generator = Generator(classifier.classify(images), deltaCalculator, builder)
-	generator.generate(source).save('output.png', compress_level=1)
+	generator.generate(source).save(outputName, compress_level=1)
 
-def getTileImages():
-	return [Image.open(path).convert('RGBA') for path in glob.glob('images/*.png')]
+def getTileImages(imagesPath):
+	return [Image.open(path).convert('RGBA') for path in glob.glob(os.path.join(imagesPath, '*.png'))]
 
 
 if __name__ == '__main__':
