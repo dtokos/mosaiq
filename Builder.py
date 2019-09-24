a63bb135de1e0c3a2cf8478c8f98ff114ea00c78
Dobroslav Tokoš <dtokos@zoznam.sk>
2019-09-24 09:21:14 +0200

    Implemented image resizing

--- Builder.py
+++ Builder.py
@@ -5,11 +5,16 @@ class Builder:
 	def __init__(self, tileSize):
 		self._image = None
 		self._tileSize = tileSize
+		self._resizeCache = None
 
 	def new(self, gridSize):
 		self._image = Image.new('RGBA', self._calculateImageSize(gridSize))
+		self._resizeCache = {}
 
 	def paste(self, image, row, column):
+		if self._tileSize != image.size:
+			image = self._resizeTile(image)
+		
 		self._image.paste(image, (row * self._tileSize[0], column * self._tileSize[1]))
 
 	def getImage(self):
@@ -17,3 +22,9 @@ class Builder:
 
 	def _calculateImageSize(self, gridSize):
 		return (gridSize[0] * self._tileSize[0], gridSize[1] * self._tileSize[1])
+
+	def _resizeTile(self, image):
+		if not image in self._resizeCache:
+			self._resizeCache[image] = image.resize(self._tileSize)
+
+		return self._resizeCache[image]
